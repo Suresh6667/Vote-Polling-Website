e76afa283f7782bc3a9565eb43b483297aa8cd40
Suresh6667 <suresh24612@gmail.com>
2025-08-22 18:09:30 +0530

    fix issues

--- server/database/init.js
+++ server/database/init.js
@@ -2,10 +2,7 @@ import sqlite3 from 'sqlite3';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const dbPath = process.env.NODE_ENV === 'production'
-  ? '/data/polls.db' // Render persistent disk
-  : join(__dirname, process.env.DB_PATH || '../data/polls.db');
+const dbPath = process.env.DB_PATH || join(__dirname, '../data/polls.db');
 
 export let db;
 
